583a14cc001062ba62d5c3a5daec7c2677dd7536
xim0608 <xim.ryuki@gmail.com>
2018-06-20 23:06:00 +0900

    initial commit

---
+++ atcoder_abc081_a/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		marbles   string
+		counter = 0
+	)
+	fmt.Scan(&marbles)
+	for _, value := range marbles {
+		if value == '1' {
+			counter++
+		}
+	}
+	fmt.Println(counter)
+}
